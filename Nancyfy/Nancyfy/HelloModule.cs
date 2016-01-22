617786d664149d6b3e32e85318dc449964b37145
Oleg Taykalo <Oleg.Taykalo@albumprinter.com>
2016-01-22 13:13:53 +0100

    nancy

---
+++ Nancyfy/Nancyfy/HelloModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nancyfy
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
---
+++ Nancyfy/Nancyfy/Program.cs
@@ -0,0 +1,21 @@
+﻿using Owin;
+using Microsoft.Owin.Hosting;
+using System;
+using Nancy.Hosting.Self;
+
+namespace Nancyfy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://+:9999";
+
+            using (var host = new NancyHost(new Uri("http://localhost:1234")))
+            {
+                host.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
